f72b43d1a804c8f5158975df933bc0c5616f7e8e
RJ Ocampo <rjqocampo@gmail.com>
2023-10-16 12:19:39 +0800

    Add show time function

--- src/index.js
+++ src/index.js
@@ -21,4 +21,47 @@ async function getWeather(city) {
   }
 }
 
+function showTime() {
+  const date = new Date();
+  const day = parseDay(date.getDay());
+  const h = date.getHours();
+  const m = date.getMinutes();
+  const s = date.getSeconds();
+
+  const h1 = document.querySelector("h1");
+
+  console.log(`${day} -- ${h}:${m}:${s}`);
+
+  h1.textContent = `${day} -- ${h}:${m}:${s}`;
+
+  setTimeout(showTime, 1000);
+}
+
+function parseDay(day) {
+  switch (day) {
+    case 1:
+      return "Mon";
+      break;
+    case 2:
+      return "Tue";
+      break;
+    case 3:
+      return "Wed";
+      break;
+    case 4:
+      return "Thu";
+      break;
+    case 5:
+      return "Fri";
+      break;
+    case 6:
+      return "Sat";
+      break;
+    case 7:
+      return "Sun";
+      break;
+  }
+}
+
 getWeather("manila");
+showTime();
