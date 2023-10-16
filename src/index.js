296d42112c425dd6e45011a2c1f0aa7df05a19b7
RJ Ocampo <rjqocampo@gmail.com>
2023-10-16 12:01:14 +0800

    Add function to get weather

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import "./styles/index.scss";
 
-console.log("hello world");
+async function getWeather(city) {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=968026c727974d82b99102459230710&q=${city}&days=3`,
+      { mode: "cors" },
+    );
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      console.log("RESPONSE: ", response);
+      console.log("DATA: ", data);
+      throw new Error(data.error.message);
+    }
+
+    console.log("RESPONSE: ", response);
+    console.log("DATA: ", data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getWeather("manila");
