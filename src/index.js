6545a60e51dc1219ff116361d3dd268e046ca184
RJ Ocampo <rjqocampo@gmail.com>
2023-10-19 19:08:04 +0800

    Add github footer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./styles/sec-one.scss";
 import "./styles/sec-two.scss";
 import "./styles/sec-three.scss";
 import "./styles/sec-four.scss";
+import "./styles/footer.scss";
 import {
   populateDate,
   populateSectionFour,
